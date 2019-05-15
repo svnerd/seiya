34fb0242bf98e560036fefb0c51b57bd899802ae
Yuan Chen <chenyuan@NYCnerds-MacBook-Air.local>
2019-05-14 22:51:10 -0400

    inception experiment

---
+++ cnn/Inception.py
@@ -0,0 +1,39 @@
+from torch import nn
+import torch.nn.functional as F
+import torch
+
+
+def inception_k5():
+    conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5, padding=2)
+    pool = nn.MaxPool2d(kernel_size=2, stride=2)
+    conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, padding=2)
+    return [conv1, pool, conv2]
+
+def inception_k3():
+    conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=3, padding=1)
+    pool = nn.MaxPool2d(kernel_size=2, stride=2)
+    conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=3, padding=1)
+    return [conv1, pool, conv2]
+
+def forward(conv1, pool, conv2, images):
+    t = F.relu(conv1(images))
+    t = pool(t)
+    return F.relu(conv2(t))
+
+class SimpleInception(nn.Module):
+    def __init__(self):
+        super(SimpleInception, self).__init__()
+        self.conv1_5, self.pool_5, self.conv2_5 = inception_k5()
+        self.conv1_3, self.pool_3, self.conv2_3 = inception_k3()
+        self.fc1 = nn.Linear(32*16*16, 16*16)
+        self.fc2 = nn.Linear(16*16, 10)
+
+    def forward(self, images):
+        o5 = forward(self.conv1_5, self.pool_5, self.conv2_5, images)
+        o3 = forward(self.conv1_3, self.pool_3, self.conv2_3, images)
+        t = torch.cat([o3, o5], dim=1)
+        t = t.reshape(-1, 32*16*16)
+        t = self.fc1.forward(t)
+        t = F.relu(t)
+        t = self.fc2.forward(t)
+        return t
--- cnn/cifar10.py
+++ cnn/cifar10.py
@@ -2,6 +2,7 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 from seiya.cnn.LeNet5 import LeNet5
+from seiya.cnn.Inception import SimpleInception
 import torch.optim as optim
 import os
 from argparse import ArgumentParser
@@ -13,14 +14,20 @@ if __name__ == '__main__':
     args = parser.parse_args()
     data_dir = args.data_dir
 
-    net = LeNet5()
+    #net = LeNet5()
+    net = SimpleInception()
     cross_entropy = torch.nn.CrossEntropyLoss()
     optimizer = optim.Adam(net.parameters(), lr=1e-4)
+    '''
     transform = transforms.Compose([
         transforms.Grayscale(num_output_channels=1),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5], std=[0.5])
     ])
+    '''
+    transform = transforms.Compose(
+        [transforms.ToTensor(), # chain 2 transforms together using list.
+         transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
 
     trainset = torchvision.datasets.CIFAR10(root=data_dir, train=True,
                                             download=True, transform=transform)
@@ -31,9 +38,9 @@ if __name__ == '__main__':
                                            download=False, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=4,
                                              shuffle=False, num_workers=2)
-    record_dir = os.path.join(data_dir, "lenet5")
+    record_dir = os.path.join(data_dir, "inception")
     os.makedirs(record_dir, exist_ok=True)
-    network_dict = {"lenet5": net}
+    network_dict = {"inception": net}
 
     sr_service = SaveRestoreService(record_dir, network_dict)
     sr_service.restore()
